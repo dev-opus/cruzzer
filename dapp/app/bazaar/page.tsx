7522bd483790bc154ed265c7f097c27ac1c216de
victor orlunda <vicorlunda@gmail.com>
2024-07-28 19:09:24 +0100

    debug: add log to handler function pt5

--- dapp/app/bazaar/page.tsx
+++ dapp/app/bazaar/page.tsx
@@ -54,7 +54,7 @@ export default function Bazaar() {
           image: 'https://' + res.image,
           price: formatEther(rawNfts[i].price),
           forSale: rawNfts[i].forSale as boolean,
-          tokenId: parseUnits(rawNfts[i].tokenId, 10),
+          tokenId: parseUnits(rawNfts[i].tokenId, 1),
           creator: rawNfts[i].owner as Address,
         };
 
